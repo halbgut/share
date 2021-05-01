7af4f94e4726b74506d93962df46bc9026d6fac0
Luca Nils Schmid <me@lucaschmid.net>
2021-05-01 18:48:43 +0200

    more aggressively validate paths

--- validate.go
+++ validate.go
@@ -6,8 +6,14 @@ import (
 )
 
 func validate(path string) error {
+	if len(path) < 2 {
+		return fmt.Errorf("Path too short")
+	}
+	if strings.Contains(path[1:], "/") {
+		return fmt.Errorf("Path may not contain '/'")
+	}
 	if strings.Contains(path, "..") {
-		return fmt.Errorf("Invalid request. Path contains '..'")
+		return fmt.Errorf("Path may not contain '..'")
 	}
 	return nil
 }
